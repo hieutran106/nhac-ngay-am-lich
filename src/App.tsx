8c9e9e1fc7a550ba6466824ef731bc941f76055d
Hieu Tran <hieutran106@gmail.com>
2026-01-06 15:59:01 +1000

    Fix react router base name

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,27 @@ import { HomePage } from "./pages/HomePage.tsx";
 import { AddPage } from "./pages/AddPage.tsx";
 import { SharedLayout } from "./pages/Layout.tsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <SharedLayout />, // Parent layout with Header
+            children: [
+                {
+                    index: true, // Matches "/"
+                    element: <HomePage />
+                },
+                {
+                    path: "add", // Matches "/add"
+                    element: <AddPage />
+                }
+            ]
+        }
+    ],
     {
-        path: "/",
-        element: <SharedLayout />, // Parent layout with Header
-        children: [
-            {
-                index: true, // Matches "/"
-                element: <HomePage />
-            },
-            {
-                path: "add", // Matches "/add"
-                element: <AddPage />
-            }
-        ]
+        basename: "/nhac-ngay-am-lich"
     }
-]);
+);
 
 export default function App() {
     return <RouterProvider router={router} />;
